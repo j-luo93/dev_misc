0696ef923bbb510c65379bced63270af12974c39
j-luo93 <j_luo@mit.edu>
2019-04-11 03:18:08 -0400

    log_this decorator

--- logger.py
+++ logger.py
@@ -11,7 +11,7 @@ class LogFormatter(ColoredFormatter):
 
     def __init__(self, color=False):
         if color:
-            fmt = '%(log_color)s%(levelname)s - %(time)s - %(elapsed)s - %(message)s%(reset)s'
+            fmt = '%(log_color)s%(levelname)s - %(time)s - %(elapsed)s at %(filename)s:%(lineno)d - %(message)s%(reset)s'
         else:
             fmt = '%(levelname)s - %(time)s - %(elapsed)s - %(message)s'
         super(LogFormatter, self).__init__(fmt)
@@ -67,3 +67,39 @@ def create_logger(filepath=None, log_level='INFO'):
     logger.reset_time = reset_time
 
     return logger
+
+def log_this(msg='', log_level='DEBUG', arg_list=None):
+    """
+    A decorator that logs the functionality, the beginning and the end of the function.
+    It can optionally print out arg values in arg_list.
+    """
+
+    def decorator(func):
+        new_msg = msg or func.__name__
+        new_arg_list = arg_list or list()
+        arg2pos = {a: i for i, a in enumerate(func.__code__.co_varnames)}
+        log_func = lambda msg: logging.log(getattr(logging, log_level), msg)
+        
+        def wrapper(*args, **kwargs):
+            log_func(f'*STARTING* {new_msg}')
+            if new_arg_list:
+                arg_msg = dict()
+                for a in new_arg_list:
+                    if a in kwargs:
+                        arg_msg[a] = kwargs[a]
+                    else:
+                        pos = arg2pos[a]
+                        if pos < len(args):
+                            arg_msg[a] = args[pos]
+                        else:
+                            arg_msg[a] = 'DEFAULT'
+
+                log_func(f'*ARG_LIST* {arg_msg}')
+
+            ret = func(*args, **kwargs)
+            log_func(f'*FINISHED* {new_msg}')
+            return ret
+
+        return wrapper
+
+    return decorator
