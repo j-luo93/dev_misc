ec6b415d7bc5e3891a6f43afcd6c74e76c628fd6
j-luo93 <j_luo@mit.edu>
2019-10-10 00:53:58 -0400

    minor

--- devlib/initiate.py
+++ devlib/initiate.py
@@ -9,7 +9,7 @@ from arglib import (Registry, add_argument, add_registry, g, get_configs,
 from trainlib import create_logger
 
 
-def initiate(registries: Iterable[Registry] = None, logger=False, log_dir=False, log_level=False, gpus=False):
+def initiate(*registries: Iterable[Registry], logger=False, log_dir=False, log_level=False, gpus=False):
     """
     This function does a few things.
     1. Hook registries to arglib.
