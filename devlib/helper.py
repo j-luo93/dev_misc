e87ad4f80853ec93f6837bd7fca8f879755baa08
j-luo93 <j_luo@mit.edu>
2019-10-27 17:27:41 -0400

    option to force to use cpu tensors in helper.py

--- devlib/helper.py
+++ devlib/helper.py
@@ -1,11 +1,17 @@
 import os
+from functools import wraps
+from typing import List, Union
 
 import numpy as np
 import torch
 
+# IDEA(j_luo) Tensor generic type!
+Tensor = torch.Tensor
+Tensorable = Union[np.ndarray, Tensor, List]
 
-def get_tensor(x):
-    """Get a tensor from x, and move it to GPU if possible."""
+
+def get_tensor(x: Tensorable, cpu=False) -> Tensor:
+    """Get a tensor from x, and move it to GPU if possible (unless `cpu` is True)."""
     # Convert it to tensor.
     if isinstance(x, np.ndarray):
         tensor = torch.from_numpy(x)
@@ -16,29 +22,31 @@ def get_tensor(x):
     else:
         raise NotImplementedError(f'Unsupported type {type(x)}.')
     # Check if we need to use cuda.
-    if os.environ.get('CUDA_VISIBLE_DEVICES', False):
+    if os.environ.get('CUDA_VISIBLE_DEVICES', False) and not cpu:
         use_cuda = True
     else:
         use_cuda = False
     if use_cuda:
         tensor = tensor.cuda()
+    else:
+        tensor = tensor.cpu()
     # NOTE(j_luo) Convert everything back to unnamed tensor so that we don't have to deal with unsupported ops on named tensors later.
-    # tensor.rename_(None)
+    tensor.rename_(None)  # TODO(j_luo) use something from named_tensor.py?
     return tensor
 
 
-def get_zeros(*shape):
+def get_zeros(*shape: int, cpu: bool = False):
     """Get zeros and move to gpu if possible."""
-    return get_tensor(torch.zeros(*shape))
+    return get_tensor(torch.zeros(*shape), cpu=cpu)
 
 
-def get_range(size, ndim, dim):
+def get_range(size: int, ndim: int, dim: int, cpu: bool = False):
     """Get torch.arange(size), and reshape it to have the shape where all `ndim` dimensions are of size 1 but the `dim` is `size`, and move it to GPU if possible."""
     shape = [1] * dim + [size] + [1] * (ndim - dim - 1)
-    return get_tensor(torch.arange(size).long().reshape(*shape))
+    return get_tensor(torch.arange(size).long().reshape(*shape), cpu=cpu)
 
 
-def pad_to_dense(a, dtype=np.float32):
+def pad_to_dense(a: List[np.ndarray], dtype=np.float32) -> np.ndarray:
     '''
     Modified from https://stackoverflow.com/questions/37676539/numpy-padding-matrix-of-different-row-size.
     '''
@@ -49,15 +57,13 @@ def pad_to_dense(a, dtype=np.float32):
     return ret
 
 
-def get_length_mask(lengths, max_len) -> torch.BoolTensor:
+def get_length_mask(lengths: Tensor, max_len: int, cpu: bool = False) -> torch.BoolTensor:
     """Get a mask that is True if the index is less than the corresponding length."""
-    if max_len < max(lengths):
-        raise RuntimeError(f'max_len too small: {max_len} < {max(lengths)}.')
     if lengths.ndim != 1:
         raise TypeError(f'Expect lengths to be a vector, but got {lengths.ndim} dimensions.')
-    mask = get_zeros(len(lengths), max_len).bool()
-    indices = get_range(max_len, 2, 1)
-    within_length = indices < get_tensor(lengths).unsqueeze(dim=-1)
+    mask = get_zeros(len(lengths), max_len, cpu=cpu).bool()
+    indices = get_range(max_len, 2, 1, cpu=cpu)
+    within_length = indices < get_tensor(lengths, cpu=cpu).unsqueeze(dim=-1)
     # TODO(j_luo) ugly
     mask[within_length] = True
     return mask
