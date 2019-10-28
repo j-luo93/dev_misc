57e54adb283128b15dae5fffa75e2f0c98b89fed
j-luo93 <j_luo@mit.edu>
2019-10-28 15:12:33 -0400

    option to automatically compute commit_id

--- devlib/initiate.py
+++ devlib/initiate.py
@@ -1,5 +1,6 @@
 import os
 import random
+import subprocess
 import time
 from datetime import datetime
 from pathlib import Path
@@ -10,27 +11,57 @@ from arglib import (Registry, add_argument, add_registry, g, get_configs,
 from trainlib import create_logger
 
 
-def initiate(*registries: Iterable[Registry], logger=False, log_dir=False, log_level=False, gpus=False, random_seed=False):
+def _is_in_git_repo() -> bool:
+    res = subprocess.run('git rev-parse --is-inside-work-tree', shell=True, capture_output=True)
+    if res.returncode == 127:
+        raise OSError('git not installed.')
+    elif res.returncode != 0:
+        raise OSError('Some git-related error.')
+    else:
+        output = res.stdout.decode('utf8').strip()
+        if output == 'true':
+            return True
+        else:
+            return False
+
+
+def _get_head_commit_id() -> str:
+    if _is_in_git_repo():
+        res = subprocess.run('git rev-parse --short HEAD', shell=True, capture_output=True)
+        if res.return_code == 0:
+            return res.stdout.decode('utf8').strip()
+        else:
+            return ''
+    return ''
+
+
+def initiate(*registries: Registry, logger=False, log_dir=False, log_level=False, gpus=False, random_seed=False, commit_id=False, stacklevel=1):
     """
     This function does a few things.
     1. Hook registries to arglib.
     2. Add a few default arguments: log_dir, log_level, message or random_seed. Note that setting up a random seed is not done by this function since there might be multiple seeds to set up.
     3. Automatically set up log_dir if not already specified and mkdir.
     4. Create a logger with proper log_level and file_path.
+    5. Add the current head commit id.
     """
     if registries:
         for reg in registries:
             add_registry(reg, stacklevel=2)
 
+    stacklevel = stacklevel + 1
     if log_dir:
-        add_argument('log_dir', dtype='path', msg='log directory', stacklevel=2)
-        add_argument('message', default='', msg='message to append to the config class name', stacklevel=2)
+        add_argument('log_dir', dtype='path', msg='log directory', stacklevel=stacklevel)
+        add_argument('message', default='', msg='message to append to the config class name', stacklevel=stacklevel)
     if log_level:
-        add_argument('log_level', default='INFO', msg='log level', stacklevel=2)
+        add_argument('log_level', default='INFO', msg='log level', stacklevel=stacklevel)
     if gpus:
-        add_argument('gpus', dtype=int, nargs='+', msg='GPUs to use', stacklevel=2)
+        add_argument('gpus', dtype=int, nargs='+', msg='GPUs to use', stacklevel=stacklevel)
     if random_seed:
-        add_argument('random_seed', dtype=int, default=1234, msg='random seed to set', stacklevel=2)
+        add_argument('random_seed', dtype=int, default=1234, msg='random seed to set', stacklevel=stacklevel)
+    if commit_id:
+        commit_id = _get_head_commit_id()
+        add_argument('commit_id', dtype=str, default=commit_id,
+                     msg='commit id of current head, automatically computed', stacklevel=stacklevel)
     parse_args(known_only=True)
 
     # Set an environment variable.
