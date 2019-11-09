a63deea8f37b498d3e410bbb52aa87aee9654b49
j-luo93 <j_luo@mit.edu>
2019-11-09 11:58:59 -0500

    cached_property

--- devlib/__init__.py
+++ devlib/__init__.py
@@ -1,5 +1,6 @@
 from .data.pandas import PandasDataLoader, PandasDataset, pandas_collate_fn
-from .helper import (check_explicit_arg, dataclass_cuda, dataclass_numpy,
-                     dataclass_size_repr, freeze, get_length_mask, get_range,
-                     get_tensor, get_trainable_params, get_zeros, pad_to_dense)
+from .helper import (cached_property, check_explicit_arg, dataclass_cuda,
+                     dataclass_numpy, dataclass_size_repr, freeze,
+                     get_length_mask, get_range, get_tensor,
+                     get_trainable_params, get_zeros, pad_to_dense)
 from .initiate import initiate
