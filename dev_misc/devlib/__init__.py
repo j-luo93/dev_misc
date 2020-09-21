5c2b50860b4cf57ddfa389e1d5ed82227b87df7f
j-luo93 <j_luo@mit.edu>
2020-09-20 21:51:23 -0400

    initiate function is broken down into two parts, executed by Initiator

--- dev_misc/devlib/__init__.py
+++ dev_misc/devlib/__init__.py
@@ -1,12 +1,12 @@
 import numpy as np
 import torch
 
+from ._initiate import Initiator
 from .dataset.pandas import PandasDataLoader, PandasDataset, pandas_collate_fn
 from .helper import (BaseBatch, batch_class, dataclass_cuda, dataclass_numpy,
                      dataclass_size_repr, debug_stats, get_array,
                      get_length_mask, get_range, get_tensor, get_zeros,
                      pad_to_dense)
-from ._initiate import initiate
 
 NDA = np.ndarray
 LT = torch.LongTensor
--- dev_misc/devlib/_initiate.py
+++ dev_misc/devlib/_initiate.py
@@ -7,8 +7,8 @@ from pathlib import Path
 from typing import Iterable
 
 from dev_misc.arglib import (Registry, add_argument, add_registry, g,
-                             get_configs, parse_args, set_argument)
-from dev_misc.trainlib import create_logger
+                             get_configs, parse_args, set_argument, show_args)
+from dev_misc.trainlib import create_logger, set_random_seeds
 
 
 def _is_in_git_repo() -> bool:
@@ -39,74 +39,90 @@ class DirAlreadyExists(Exception):
     """Raise this error if the specified `log_dir` already exists."""
 
 
-def initiate(*registries: Registry,
-             logger=False, log_dir=False, log_level=False,
-             gpus=False, random_seed=False, commit_id=False,
-             stacklevel=1):
-    """
-    This function does a few things.
-    1. Hook registries to arglib.
-    2. Add a few default arguments: log_dir, log_level, message or random_seed. Note that setting up a random seed is not done by this function since there might be multiple seeds to set up.
-    3. Automatically set up log_dir if not already specified and mkdir.
-    4. Create a logger with proper log_level and file_path.
-    5. Add the current head commit id.
-    """
-    if registries:
-        for reg in registries:
-            add_registry(reg, stacklevel=2)
+class Initiator:
 
-    stacklevel = stacklevel + 1
-    if log_dir:
-        add_argument('log_dir', dtype='path', msg='log directory', stacklevel=stacklevel)
-        add_argument('message', default='', msg='message to append to the config class name', stacklevel=stacklevel)
-    if log_level:
-        add_argument('log_level', default='INFO', msg='log level', stacklevel=stacklevel)
-    if gpus:
-        add_argument('gpus', dtype=int, nargs='+', msg='GPUs to use', stacklevel=stacklevel)
-    if random_seed:
-        add_argument('random_seed', dtype=int, default=1234, msg='random seed to set', stacklevel=stacklevel)
-    if commit_id:
-        commit_id = _get_head_commit_id()
-        add_argument('commit_id', dtype=str, default=commit_id,
-                     msg='commit id of current head, automatically computed', stacklevel=stacklevel)
-    # FIXME(j_luo) The following doesn't do what I wanted. `initiate` is usually called after importing packages, which already declares many arguments before `log_dir` and others. We need to specify the group of parameters to parse instead of known only.
-    parse_args(known_only=True)
+    def __init__(self, *registries: Registry,
+                 logger=False, log_dir=False, log_level=False,
+                 gpus=False, random_seed=False, commit_id=False,
+                 stacklevel=1):
+        """
+        This function does a few things.
+        1. Hook registries to arglib.
+        2. Add a few default arguments: log_dir, log_level, message or random_seed. Note that setting up a random seed is not done by this function since there might be multiple seeds to set up.
+        3. Automatically set up log_dir if not already specified and mkdir.
+        4. Create a logger with proper log_level and file_path.
+        5. Add the current head commit id.
+        """
+        if registries:
+            for reg in registries:
+                add_registry(reg, stacklevel=2)
 
-    # Set an environment variable.
-    if gpus and g.gpus:
-        # NOTE(j_luo) This environment variable is a string.
-        os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(map(str, g.gpus))
+        stacklevel = stacklevel + 1
+        if log_dir:
+            add_argument('log_dir', dtype='path', msg='log directory', stacklevel=stacklevel)
+            add_argument('message', default='', msg='message to append to the config class name', stacklevel=stacklevel)
+        if log_level:
+            add_argument('log_level', default='INFO', msg='log level', stacklevel=stacklevel)
+        if gpus:
+            add_argument('gpus', dtype=int, nargs='+', msg='GPUs to use', stacklevel=stacklevel)
+        if random_seed:
+            add_argument('random_seed', dtype=int, default=1234, msg='random seed to set', stacklevel=stacklevel)
+        if commit_id:
+            commit_id = _get_head_commit_id()
+            add_argument('commit_id', dtype=str, default=commit_id,
+                         msg='commit id of current head, automatically computed', stacklevel=stacklevel)
 
-    # log_dir would be automatically set as follows if it is not specified manually:
-    # ./log/<date>/<config_class_name>[-<message>]/<timestamp>
-    if log_dir:
-        if g.log_dir:
-            try:
-                g.log_dir.mkdir(parents=True)
-            except FileExistsError:
-                raise DirAlreadyExists(f'Directory {g.log_dir} already exists.')
-        else:
-            folder = Path('./log')
-            configs = get_configs()
-            identifier = '-'.join(filter(lambda x: x is not None, configs.values()))
-            identifier = identifier or 'default'
-            if g.message:
-                identifier += f'-{g.message}'
+        self.log_dir = log_dir
+        self.log_level = log_level
+        self.logger = logger
+        self.random_seed = random_seed
+        self.gpus = gpus
 
-            while True:
-                now = datetime.now()
-                date = now.strftime(r"%Y-%m-%d")
-                timestamp = now.strftime(r"%H-%M-%S")
-                log_dir = folder / date / identifier / timestamp
-                if log_dir.exists():
-                    time.sleep(1)
-                else:
-                    set_argument('log_dir', log_dir, _force=True)
-                    log_dir.mkdir(parents=True)
-                    break
+    def run(self):
+        parse_args()
+        # parse_args(known_only=True)
 
-    # Create a logger.
-    if logger:
-        file_path = Path(g.log_dir) / 'log' if log_dir else None
-        log_level = g.log_level if log_level else 'INFO'
-        create_logger(file_path=file_path, log_level=log_level)
+        # Set an environment variable.
+        if self.gpus and g.gpus:
+            # NOTE(j_luo) This environment variable is a string.
+            os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(map(str, g.gpus))
+
+        # log_dir would be automatically set as follows if it is not specified manually:
+        # ./log/<date>/<config_class_name>[-<message>]/<timestamp>
+        if self.log_dir:
+            if g.log_dir:
+                try:
+                    g.log_dir.mkdir(parents=True)
+                except FileExistsError:
+                    raise DirAlreadyExists(f'Directory {g.log_dir} already exists.')
+            else:
+                folder = Path('./log')
+                configs = get_configs()
+                identifier = '-'.join(filter(lambda x: x is not None, configs.values()))
+                identifier = identifier or 'default'
+                if g.message:
+                    identifier += f'-{g.message}'
+
+                while True:
+                    now = datetime.now()
+                    date = now.strftime(r"%Y-%m-%d")
+                    timestamp = now.strftime(r"%H-%M-%S")
+                    log_dir = folder / date / identifier / timestamp
+                    if log_dir.exists():
+                        time.sleep(1)
+                    else:
+                        set_argument('log_dir', log_dir, _force=True)
+                        log_dir.mkdir(parents=True)
+                        break
+
+        # Create a logger.
+        if self.logger:
+            file_path = Path(g.log_dir) / 'log' if self.log_dir else None
+            log_level = g.log_level if self.log_level else 'INFO'
+            create_logger(file_path=file_path, log_level=log_level)
+
+        # Set random seed.
+        if self.random_seed:
+            set_random_seeds(g.random_seed)
+
+        show_args()
